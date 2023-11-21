f06f995d9b4ef51e4f3c1ab867201a5fa4f92c02
MauroAlexandro <mauroalexandro@gmail.com>
2023-11-21 18:21:19 +0000

    Test to Utils saving and returning a list from SharedPreferences

--- app/src/androidTest/java/com/mauroalexandro/dogsareawesome/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/mauroalexandro/dogsareawesome/ExampleInstrumentedTest.kt
@@ -2,6 +2,10 @@ package com.mauroalexandro.dogsareawesome
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.mauroalexandro.dogsareawesome.models.Breed
+import com.mauroalexandro.dogsareawesome.models.Height
+import com.mauroalexandro.dogsareawesome.models.Weight
+import com.mauroalexandro.dogsareawesome.utils.Utils
 
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -15,10 +19,38 @@ import org.junit.Assert.*
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
+    private var utils: Utils = Utils.getInstance()
     @Test
-    fun useAppContext() {
+    fun listSavedToSharedPreferencesReturned() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.mauroalexandro.dogsareawesome", appContext.packageName)
+
+        val testKey = "TEST"
+
+        //List to send to SharedPreferences
+        val breedList = ArrayList<Breed>()
+
+        val breedHeight = Height("1","1")
+        val breedWeight = Weight("1","1")
+        val breed = Breed(
+            "hunting",
+            "hunters",
+            breedHeight,
+            0,
+            "100",
+            "Dog",
+            "Space",
+            "NoNe",
+            "Easy Going",
+            breedWeight
+        )
+
+        breedList.add(breed)
+        utils.setList(testKey,breedList, appContext)
+
+        //List got from SharedPreferences
+        val outputList = utils.getList(testKey)
+
+        assertTrue(outputList.containsAll(breedList) && breedList.containsAll(outputList))
     }
 }
